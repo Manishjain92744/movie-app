d5ce6c78424f55693dd63122d317c0e435211853
manishkumar25031999 <manishkumar25031999@gmail.com>
2022-08-08 10:48:48 +0530

    changing state to add favourite as well

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,33 @@
-import { ADD_MOVIES } from "../actions";
+import { ADD_MOVIES ,ADD_FAVOURITE} from "../actions";
 
 const initialMoviesState = {
     list : [],
-    Favourites : []
+    favourites : []
 } 
 
 export default function movies(state = initialMoviesState,action){
-    if(action.type === 'ADD_MOVIES')
-    {
-        return {
+
+    // if(action.type === 'ADD_MOVIES')    bcz react community used switch case instaed of if else 
+    // {
+    //     return {
+    //         ...state,
+    //        list : action.movies
+    //     }
+    // }
+    // return state;
+
+    switch(action.type) {
+    case ADD_MOVIES :
+       return {
+          ...state,
+          list : action.movies
+       }
+       case ADD_FAVOURITE:
+          return { 
             ...state,
-           list : action.movies
+            favourites : [action.movie,...state.favourites]
+          }
+          default : 
+          return state; 
         }
-    }
-    return state;
 }

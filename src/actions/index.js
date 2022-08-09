cee519614ce805b80a61fb55ccd1ca67d8dd2f69
manishkumar25031999 <manishkumar25031999@gmail.com>
2022-08-09 21:40:32 +0530

    added search list

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,8 @@ export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_FAVOURITE = 'ADD_FAVOURITE';
 export const REMOVE_FAVOURITE = 'REMOVE_FAVOURITE';
 export const SET_SHOW_FAVOURITE = 'SET_SHOW_FAVOURITE';
+ export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
+ export const ADD_SEARCH_RESULT = 'ADD_SEARCH_RESULT';
 
 
 
@@ -34,4 +36,30 @@ export function setShowFavourite (val) {
         type : SET_SHOW_FAVOURITE ,
         val
     }
+}
+export function  addMovieToList(movie) {
+    return {
+        type : ADD_MOVIE_TO_LIST ,
+        movie
+    }
+}
+
+ export function handleMovieSearch(movie ) {
+    const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${movie}`;
+   return function (dispatch) {
+    fetch(url)
+    .then (response => response.json())
+    .then (movie => {
+        console.log('movie',movie);
+        // dispatch an action
+        dispatch(addMovieSearchResult(movie))
+    })
+ }
+}
+
+export function addMovieSearchResult(movie) {
+    return {
+        type : ADD_SEARCH_RESULT,
+        movie
+    };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,8 @@ import { ADD_MOVIES ,
    ADD_FAVOURITE,
    REMOVE_FAVOURITE,
    SET_SHOW_FAVOURITE,
-  // ADD_MOVIE_TO_LIST
+   ADD_MOVIE_TO_LIST,
+   ADD_SEARCH_RESULT
 } from "../actions";
 
 const initialMoviesState = {
@@ -49,11 +50,11 @@ export  function movies(state = initialMoviesState,action){
             ...state,
             showFavourites : action.val
           }
-         //  case ADD_MOVIE_TO_LIST : 
-         //    return {
-         //       ...state,
-         //       list : [action.movie,...state.list]
-         //    };
+          case ADD_MOVIE_TO_LIST : 
+            return {
+               ...state,
+               list : [action.movie,...state.list]
+            };
         
       default:
          return state;
@@ -61,12 +62,26 @@ export  function movies(state = initialMoviesState,action){
 }
 
 const initialSearchState = {
-   result : {}
+   result : {},
+   showSearchResults : false
 }
 export function search (state = initialSearchState , action) {
-   console.log('Search Reducer');
 
+   switch(action.type) {
+      case ADD_SEARCH_RESULT : 
+      return {
+         ...state,
+         result : action.movie,
+         showSearchResults : true
+      }
+      case ADD_MOVIE_TO_LIST : 
+            return {
+               ...state,
+               showSearchResults : false
+            };
+     default :
    return state;
+}
 }
 const initialRootState = {
    movies : initialMoviesState,
